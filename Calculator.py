87475db99bd9073795f4443d95d419d9ca8b5eeb
oskareek <oskar.eek99@outlook.com>
2024-05-13 23:20:52 +0200

    Initial code for bearish divergance checking

--- Calculator.py
+++ Calculator.py
@@ -3,7 +3,7 @@ from scipy.signal import argrelextrema
 
 class Calculator:
 
-    def calulate_rsi(self, gains, loss, currentGain, currentLoss):
+    def calulate_rsi(gains, loss, currentGain, currentLoss):
         if len(gains) >= 13:
             # Get the last 13 recorded gains and losses
             lastGains = gains[-13:]
@@ -25,10 +25,42 @@ class Calculator:
 
         return 0
 
-    def is_negative_divergance():
+    def is_bearish_divergence(datapoints, nrOfPeaks):
+        #There is bearish divergence if rsi make lower highs and stock prive makes higher highs
+
+        numberOfDataPoints = 31 * 7 #31 days, 7 datapoints on the hourly chart each day
+        selectedDatapoints = datapoints[-numberOfDataPoints:]
+        rsiDataPoints = [x.RSI for x in selectedDatapoints]
+
+        rsiPI = Calculator.find_list_peaks(rsiDataPoints, 10) 
+        if len(rsiPI) < nrOfPeaks:
+            return False
+        rsiPI = rsiPI.sort()
+        rsiPI = rsiPI[-nrOfPeaks:]
+
+        rsiLowerHighs = True
+        for i in range(0, len(rsiPI) - 1):
+            rsiIndex = rsiPI[i]
+            if rsiDataPoints[rsiIndex] < rsiDataPoints[rsiIndex + 1]:  #Should I have just "<" or "<=" here? 
+                rsiLowerHighs = False
+                break
+
+        if not rsiLowerHighs:
+            return False
+        
+        #TODO: Check for higher highs in stock price
+        priceDataPoints = [x.Price for x in selectedDatapoints]
         pass
 
-    def is_positive_divergence():
+
+
+    def is_bullish_divergence():
+        pass
+
+    def find_resistance():
+        pass
+
+    def find_support():
         pass
 
     def find_list_peaks(values, order=5):
