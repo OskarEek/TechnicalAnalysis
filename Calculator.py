0e945011b5da14592a2b0df91073c6f175d752f1
oskareek <oskar.eek99@outlook.com>
2024-05-14 20:51:29 +0200

    Implemented bullish divrgence and bearish divergence

--- Calculator.py
+++ Calculator.py
@@ -30,32 +30,68 @@ class Calculator:
 
         numberOfDataPoints = 31 * 7 #31 days, 7 datapoints on the hourly chart each day
         selectedDatapoints = datapoints[-numberOfDataPoints:]
-        rsiDataPoints = [x.RSI for x in selectedDatapoints]
 
-        rsiPI = Calculator.find_list_peaks(rsiDataPoints, 10) 
+        rsiDataPoints = [x.RSI for x in selectedDatapoints]
+        rsiPI = Calculator.find_list_peaks(rsiDataPoints, 10) #May need to tweek the order (10) 
         if len(rsiPI) < nrOfPeaks:
             return False
         rsiPI = rsiPI.sort()
         rsiPI = rsiPI[-nrOfPeaks:]
 
-        rsiLowerHighs = True
         for i in range(0, len(rsiPI) - 1):
             rsiIndex = rsiPI[i]
             if rsiDataPoints[rsiIndex] < rsiDataPoints[rsiIndex + 1]:  #Should I have just "<" or "<=" here? 
-                rsiLowerHighs = False
-                break
+                return False
 
-        if not rsiLowerHighs:
-            return False
-        
-        #TODO: Check for higher highs in stock price
+
         priceDataPoints = [x.Price for x in selectedDatapoints]
-        pass
+        pricePI = Calculator.find_list_peaks(priceDataPoints, 10) #May need to tweek the order (10)
+        if len(pricePI) < nrOfPeaks:
+            return False
+        pricePI = pricePI.sort()
+        pricePI = pricePI[-nrOfPeaks:]
+
+        for i in range(0, len(pricePI) - 1):
+            priceIndex = pricePI[i]
+            if priceDataPoints[priceIndex] > priceIndex[priceIndex + 1]:
+                return False
+                
+        return True
 
 
 
-    def is_bullish_divergence():
-        pass
+    def is_bullish_divergence(datapoints, nrOfPeaks):
+        #There is bullish divergence if stock price makes lower lows and RSI makes higher lows
+
+        numberOfDataPoints = 31 * 7 #31 days, 7 datapoints on the hourly chart each day
+        selectedDatapoints = datapoints[-numberOfDataPoints:]
+
+        rsiDataPoints = [x.RSI for x in selectedDatapoints]
+        rsiLI= Calculator.find_list_lows(rsiDataPoints, 10) #May need to tweek the order (10) 
+        if len(rsiLI) < nrOfPeaks:
+            return False
+        rsiLI = rsiLI.sort()
+        rsiLI = rsiLI[-nrOfPeaks:]
+
+        for i in range(0, len(rsiLI) - 1):
+            rsiIndex = rsiLI[i]
+            if rsiDataPoints[rsiIndex] < rsiDataPoints[rsiIndex + 1]:  #Should I have just "<" or "<=" here? 
+                return False
+
+
+        priceDataPoints = [x.Price for x in selectedDatapoints]
+        priceLI = Calculator.find_list_lows(priceDataPoints, 10) #May need to tweek the order (10)
+        if len(priceLI) < nrOfPeaks:
+            return False
+        priceLI = priceLI.sort()
+        priceLI = priceLI[-nrOfPeaks:]
+
+        for i in range(0, len(priceLI) - 1):
+            priceIndex = priceLI[i]
+            if priceDataPoints[priceIndex] > priceIndex[priceIndex + 1]:
+                return False
+                
+        return True
 
     def find_resistance():
         pass
